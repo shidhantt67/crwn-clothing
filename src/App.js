47f5f425cce3cf3dde531440d1874b0014f1b8be
Shidhant Thapa <shidhantt67@gmail.com>
2021-01-03 02:03:07 +0530

    react-router added and worked on shop pages and shop components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Homepage from './pages/homepage/homepage.component'
+import "./App.css";
+
+import Homepage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <Homepage />
+    
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
