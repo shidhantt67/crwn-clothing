28975d1baabae8bb7c26a6b9808c9ab4206b1565
Shidhant Thapa <shidhantt67@gmail.com>
2021-01-06 17:44:56 +0530

    cart-dropdown toggle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import "./header.style.scss";
 import { auth } from "../../firebase/firebase.conf";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -27,12 +29,20 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
+    {
+      hidden ? null
+      :
+      (<CartDropDown />)
+    }
+    
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+  currentUser: currentUser,
+  hidden: hidden
 });
 
 export default connect(mapStateToProps)(Header);
